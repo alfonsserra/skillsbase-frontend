0439ed3255135e3e90e4ba7022354d7e599d0bd2
alfonsserra <alfonsoserra@gmail.com>
2018-06-10 21:40:50 +0200

    Summary by users

--- src/app/shared/api/summary.service.ts
+++ src/app/shared/api/summary.service.ts
@@ -5,6 +5,7 @@ import { BASE_PATH } from '../variables';
 import { ApiGlobalsService } from '../../globals/globals.service';
 import { BaseService } from './base.service';
 import { OrganizationSummary } from '../model/organization-summary';
+import { UserRateSummary } from '../model/user-rate-summary';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +24,13 @@ export class SummaryService extends BaseService {
   public getOrganizationSummary(): Observable<OrganizationSummary> {
 
     return this.httpClient.get<any>(`${this.basePath}/summary/organization`, {
+      headers: this.getAuthorizationHeader()
+    });
+  }
+
+  public getUserRateSummary(skillId: number): Observable<UserRateSummary> {
+
+    return this.httpClient.get<any>(`${this.basePath}/summary/users/skill/` + skillId, {
       headers: this.getAuthorizationHeader()
     });
   }
