55833eb4a92f15d75f5bc579a87748d6d3f41a18
alfonsserra <alfonsoserra@gmail.com>
2018-06-11 16:20:20 +0200

    Add a service for the skills tree

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,13 +5,13 @@ import { MessagePopupService } from 'systelab-components/widgets/modal/message-p
 import { SkillService } from '../shared/api/skill.service';
 import { Skill } from '../shared/model/skill';
 import { AssessmentService } from '../shared/api/assessment.service';
-import { SkillAssessment } from '../shared/model/skill-assessment';
 import { CompareComponent } from './compare/compare.component';
 import { NavbarItem } from 'systelab-components/widgets/navbar/navbar.component';
 import { timer } from 'rxjs/index';
 import { SummaryService } from '../shared/api/summary.service';
 import { OrganizationSummary } from '../shared/model/organization-summary';
 import { TopUsersComponent } from './top-users/top-users.component';
+import { SkillsTreeService } from './skills-tree.service';
 
 @Component({
   selector:    'main',
@@ -32,12 +32,11 @@ export class MainComponent implements OnInit {
   public itemsNav: NavbarItem[] = [];
 
   constructor(private router: Router, protected messagePopupService: MessagePopupService,
-              protected i18nService: I18nService,
+              protected i18nService: I18nService, protected skillsTreeService: SkillsTreeService,
               protected assessmentService: AssessmentService, protected skillService: SkillService, protected summaryService: SummaryService) {
   }
 
   public ngOnInit() {
-
     this.itemsNav.push(new NavbarItem(0, 'Assessment', '', false, true, true, () => this.selectNav(0)));
     this.itemsNav.push(new NavbarItem(1, 'Organization', '', false, false, true, () => this.selectNav(1)));
     this.itemsNav.push(new NavbarItem(2, 'Top ten', '', false, false, true, () => this.selectNav(2)));
@@ -49,81 +48,14 @@ export class MainComponent implements OnInit {
       .subscribe(
         (skill) => {
           this.topSkill = skill;
-          this.initSkill(this.topSkill);
-          this.loadUserAssessment();
-          this.loadOrganizationSummary();
+          this.skillsTreeService.initSkill(this.topSkill);
+          this.skillsTreeService.loadUserAssessment(this.topSkill);
+          this.skillsTreeService.loadOrganizationSummary(this.topSkill)
+            .subscribe(
+              (sm) => this.organizationSummary = sm
+            );
         }
       );
-  }
-
-  private loadUserAssessment() {
-    this.assessmentService.getUserAssessment()
-      .subscribe(
-        (myAssessment: Array<SkillAssessment>) => {
-          for (let i = 0; i < myAssessment.length; i++) {
-            this.setSkill(this.topSkill, myAssessment[i].id.skillId, myAssessment[i].proficiency, myAssessment[i].interest, true);
-          }
-        });
-  }
-
-  private loadOrganizationSummary() {
-    this.summaryService.getOrganizationSummary()
-      .subscribe(
-        (organizationSummary) => {
-          this.organizationSummary=organizationSummary;
-          if (organizationSummary.topTenByProficiency) {
-            for (let i = 0; i < organizationSummary.topTenByProficiency.length; i++) {
-              this.setSkill(this.topSkill, organizationSummary.topTenByProficiency[i].id, organizationSummary.topTenByProficiency[i].rate, undefined, false);
-            }
-          }
-          if (organizationSummary.topTenByInterest) {
-            for (let i = 0; i < organizationSummary.topTenByInterest.length; i++) {
-              this.setSkill(this.topSkill, organizationSummary.topTenByInterest[i].id, undefined, organizationSummary.topTenByInterest[i].rate, false);
-            }
-          }
-        }
-      );
-  }
-
-  private initSkill(startingSkill: Skill) {
-    startingSkill.proficiency = 0;
-    startingSkill.interest = 0;
-
-    if (startingSkill.children) {
-      for (let i = 0; i < startingSkill.children.length; i++) {
-        this.initSkill(startingSkill.children[i]);
-      }
-    }
-  }
-
-  private setSkill(startingSkill: Skill, id: number, proficiency: number, interest: number, individual: boolean): boolean {
-    if (startingSkill.id === id) {
-      if (individual) {
-        if (proficiency) {
-          startingSkill.proficiency = proficiency;
-        }
-        if (interest) {
-          startingSkill.interest = interest;
-        }
-      } else {
-        if (proficiency) {
-          startingSkill.averageProficiency = proficiency;
-        }
-        if (interest) {
-          startingSkill.averageInterest = interest;
-        }
-      }
-      return true;
-    } else {
-      if (startingSkill.children) {
-        for (let i = 0; i < startingSkill.children.length; i++) {
-          if (this.setSkill(startingSkill.children[i], id, proficiency, interest, individual)) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
   }
 
   public skillSelected(skill: Skill) {
---
+++ src/app/main/skills-tree.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { Skill } from '../shared/model/skill';
+import { SkillAssessment } from '../shared/model/skill-assessment';
+import { AssessmentService } from '../shared/api/assessment.service';
+import { SkillService } from '../shared/api/skill.service';
+import { SummaryService } from '../shared/api/summary.service';
+import { Observable } from 'rxjs/Rx';
+import { OrganizationSummary } from '../shared/model/organization-summary';
+import { map } from 'rxjs/internal/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkillsTreeService {
+
+  constructor(protected assessmentService: AssessmentService, protected skillService: SkillService, protected summaryService: SummaryService) {
+
+  }
+
+  public initSkill(startingSkill: Skill) {
+    startingSkill.proficiency = 0;
+    startingSkill.interest = 0;
+
+    if (startingSkill.children) {
+      for (let i = 0; i < startingSkill.children.length; i++) {
+        this.initSkill(startingSkill.children[i]);
+      }
+    }
+  }
+
+  public setSkill(startingSkill: Skill, id: number, proficiency: number, interest: number, individual: boolean): boolean {
+    if (startingSkill.id === id) {
+      if (individual) {
+        if (proficiency) {
+          startingSkill.proficiency = proficiency;
+        }
+        if (interest) {
+          startingSkill.interest = interest;
+        }
+      } else {
+        if (proficiency) {
+          startingSkill.averageProficiency = proficiency;
+        }
+        if (interest) {
+          startingSkill.averageInterest = interest;
+        }
+      }
+      return true;
+    } else {
+      if (startingSkill.children) {
+        for (let i = 0; i < startingSkill.children.length; i++) {
+          if (this.setSkill(startingSkill.children[i], id, proficiency, interest, individual)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  public loadUserAssessment(startingSkill: Skill) {
+    this.assessmentService.getUserAssessment()
+      .subscribe(
+        (myAssessment: Array<SkillAssessment>) => {
+          for (let i = 0; i < myAssessment.length; i++) {
+            this.setSkill(startingSkill, myAssessment[i].id.skillId, myAssessment[i].proficiency, myAssessment[i].interest, true);
+          }
+        });
+  }
+
+  public loadOrganizationSummary(startingSkill: Skill): Observable<OrganizationSummary> {
+    return this.summaryService.getOrganizationSummary()
+      .pipe(
+        map(
+          (organizationSummary) => {
+            if (organizationSummary.topTenByProficiency) {
+              for (let i = 0; i < organizationSummary.topTenByProficiency.length; i++) {
+                this.setSkill(startingSkill, organizationSummary.topTenByProficiency[i].id, organizationSummary.topTenByProficiency[i].rate, undefined, false);
+              }
+            }
+            if (organizationSummary.topTenByInterest) {
+              for (let i = 0; i < organizationSummary.topTenByInterest.length; i++) {
+                this.setSkill(startingSkill, organizationSummary.topTenByInterest[i].id, undefined, organizationSummary.topTenByInterest[i].rate, false);
+              }
+            }
+            return organizationSummary;
+          }
+        ));
+  }
+
+}
